c06155176f2c2949c35919e24176370e2180e2f2
void pup <pupvoid210@gmail.com>
2026-01-16 16:23:36 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { base44 } from '@/api/base44Client';
 import { useQuery } from '@tanstack/react-query';
+import { toast } from 'sonner';
 import { 
   Flame, Activity, DollarSign, Droplet, X, Ban, 
   TrendingUp, Calendar, Play, Settings, ChevronRight, Coins, Sparkles, Trophy, Video, User
