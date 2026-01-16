0f9af360bb92491d53f0c32e29523fca091797bd
void pup <pupvoid210@gmail.com>
2026-01-16 17:33:21 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -2,18 +2,19 @@ import AICoach from './pages/AICoach';
 import Achievements from './pages/Achievements';
 import BullyChat from './pages/BullyChat';
 import BuyCoins from './pages/BuyCoins';
+import FindomAI from './pages/FindomAI';
 import FindomAnalytics from './pages/FindomAnalytics';
 import FindomSession from './pages/FindomSession';
 import GoonSession from './pages/GoonSession';
 import GoonerCam from './pages/GoonerCam';
 import History from './pages/History';
-import Home from './pages/Home';
 import KinkSessions from './pages/KinkSessions';
 import PremiumFindom from './pages/PremiumFindom';
 import Profile from './pages/Profile';
 import Settings from './pages/Settings';
 import Shop from './pages/Shop';
-import FindomAI from './pages/FindomAI';
+import Home from './pages/Home';
+import GoonFuel from './pages/GoonFuel';
 import __Layout from './Layout.jsx';
 
 
@@ -22,18 +23,19 @@ export const PAGES = {
     "Achievements": Achievements,
     "BullyChat": BullyChat,
     "BuyCoins": BuyCoins,
+    "FindomAI": FindomAI,
     "FindomAnalytics": FindomAnalytics,
     "FindomSession": FindomSession,
     "GoonSession": GoonSession,
     "GoonerCam": GoonerCam,
     "History": History,
-    "Home": Home,
     "KinkSessions": KinkSessions,
     "PremiumFindom": PremiumFindom,
     "Profile": Profile,
     "Settings": Settings,
     "Shop": Shop,
-    "FindomAI": FindomAI,
+    "Home": Home,
+    "GoonFuel": GoonFuel,
 }
 
 export const pagesConfig = {
